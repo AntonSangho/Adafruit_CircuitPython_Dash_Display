724f28c27ccda3c51d22de5074c670de59dcad54
AntonSangho <sanghoemail@gmail.com>
2023-08-08 11:25:53 +0900

    Add secrets.py

---
+++ secrets.py
@@ -0,0 +1,10 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'insert your network name here',
+    'password' : 'insert your network password here',
+    'aio_username' : "your-aio-username-here",
+    'aio_key' : 'your-aio-key-here',
+    'timezone' : "America/New_York"
+}
